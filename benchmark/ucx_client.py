0cb8aa6200318681c73990dff45eb402c3f7d119
Ishkhan Nazaryan <105867377+ishkhan42@users.noreply.github.com>
2023-01-19 16:02:14 +0400

    Fix: ucx communication

--- benchmark/ucx_client.py
+++ benchmark/ucx_client.py
@@ -10,6 +10,7 @@ from benchmark import benchmark_request
 triplet = np.empty(3, dtype='u4')
 host = ucp.get_address()
 port = 13337
+port_reuse = 13338
 
 def sync(to_await):
     """Waits until async operation is completed, as if it was synchronous"""
@@ -20,22 +21,39 @@ def sync(to_await):
         async_response.append(r)
 
     loop = asyncio.get_event_loop()
-    coroutine = run_and_capture_result()
-    loop.run_until_complete(coroutine)
+    loop.run_until_complete(run_and_capture_result())
     return async_response[0]
 
-async def request_sum(endpoint: ucp.Endpoint):
+async def request_sum_reuse(endpoint: ucp.Endpoint):
     triplet[0] = random.randint(1, 1000)
     triplet[1] = random.randint(1, 1000)    
     await endpoint.send(triplet[:2])
     await endpoint.recv(triplet[2:])
     assert triplet[0] + triplet[1] == triplet[2], 'Wrong sum'
 
-def main():
-    endpoint = sync(ucp.create_endpoint(host, port))
-    benchmark_request(lambda: sync(request_sum(endpoint)))
+
+async def request_sum():
+    endpoint = await ucp.create_endpoint(host, port)
+    triplet[0] = random.randint(1, 1000)
+    triplet[1] = random.randint(1, 1000)    
+    await endpoint.send(triplet[:2])
+    await endpoint.recv(triplet[2:])
+    assert triplet[0] + triplet[1] == triplet[2], 'Wrong sum'
+    await endpoint.close()
+
+
+def bench_reusing():
+    endpoint = sync(ucp.create_endpoint(host, port_reuse))
+    benchmark_request(lambda: sync(request_sum_reuse(endpoint)))
     sync(endpoint.close())
 
 
+def bench_creating():
+    benchmark_request(lambda: sync(request_sum()))
+
+
 if __name__ == '__main__':
-    main()
+    print("Creating")
+    bench_creating()
+    print("Reuseing")
+    bench_reusing()
--- benchmark/ucx_server.py
+++ benchmark/ucx_server.py
@@ -6,7 +6,16 @@ import numpy as np
 
 triplet = np.empty(3, dtype='u4')
 port = 13337
+port_reuse = 13338
 listener = None
+listener_reuse = None
+
+
+async def respond_reusers(endpoint: ucp.Endpoint):
+    while True:
+        await endpoint.recv(triplet[:2])
+        triplet[2] = triplet[0] + triplet[1]
+        await endpoint.send(triplet[2:])
 
 
 async def respond(endpoint: ucp.Endpoint):
@@ -16,9 +25,10 @@ async def respond(endpoint: ucp.Endpoint):
 
 
 async def main():
-    global listener
+    global listener, listener_reuse
     listener = ucp.create_listener(respond, port)
-    while not listener.closed():
+    listener_reuse = ucp.create_listener(respond_reusers, port_reuse)
+    while not listener.closed() and not listener_reuse.closed():
         await asyncio.sleep(0.001)
 
 
@@ -27,3 +37,4 @@ if __name__ == '__main__':
         asyncio.run(main())
     except KeyboardInterrupt:
         listener.close()
+        listener_reuse.close()
