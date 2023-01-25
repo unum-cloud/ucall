d3ea8cc40c642ec0c739653369a50c6582adba37
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-01-25 12:43:55 +0400

    Refactor: Encapsulation and default constructors

--- src/helpers/shared.hpp
+++ src/helpers/shared.hpp
@@ -34,10 +34,16 @@ struct named_callback_t {
     ujrpc_callback_t callback{};
 };
 
-template <typename element_at> struct buffer_gt {
+template <typename element_at> class buffer_gt {
     element_at* elements_{};
     std::size_t capacity_{};
     static_assert(std::is_nothrow_default_constructible<element_at>());
+
+  public:
+    buffer_gt() = default;
+    buffer_gt(buffer_gt&&) = delete;
+    buffer_gt(buffer_gt const&) = delete;
+    buffer_gt& operator=(buffer_gt const&) = delete;
 
     buffer_gt& operator=(buffer_gt&& other) noexcept {
         std::swap(elements_, other.elements_);
@@ -64,12 +70,20 @@ template <typename element_at> struct buffer_gt {
     std::size_t size() const noexcept { return capacity_; }
     std::size_t capacity() const noexcept { return capacity_; }
     element_at& operator[](std::size_t i) noexcept { return elements_[i]; }
+    element_at const& operator[](std::size_t i) const noexcept { return elements_[i]; }
 };
 
-template <typename element_at> struct array_gt {
+template <typename element_at> class array_gt {
     element_at* elements_{};
     std::size_t count_{};
     std::size_t capacity_{};
+    static_assert(std::is_nothrow_default_constructible<element_at>());
+
+  public:
+    array_gt() = default;
+    array_gt(array_gt&&) = delete;
+    array_gt(array_gt const&) = delete;
+    array_gt& operator=(array_gt const&) = delete;
 
     array_gt& operator=(array_gt&& other) noexcept {
         std::swap(elements_, other.elements_);
@@ -79,8 +93,11 @@ template <typename element_at> struct array_gt {
     }
     bool reserve(std::size_t n) noexcept {
         elements_ = (element_at*)std::malloc(sizeof(element_at) * n);
-        capacity_ = elements_ ? n : 0;
-        return elements_;
+        if (!elements_)
+            return false;
+        capacity_ = n;
+        std::uninitialized_default_construct(elements_, elements_ + capacity_);
+        return true;
     }
     ~array_gt() noexcept {
         if constexpr (!std::is_trivially_destructible<element_at>())
@@ -97,11 +114,18 @@ template <typename element_at> struct array_gt {
     void push_back(element_at&& element) noexcept { new (elements_ + count_++) element_at(element); }
 };
 
-template <typename element_at> struct pool_gt {
+template <typename element_at> class pool_gt {
     std::size_t capacity_{};
     std::size_t free_count_{};
     element_at* elements_{};
     std::size_t* free_offsets_{};
+    static_assert(std::is_nothrow_default_constructible<element_at>());
+
+  public:
+    pool_gt() = default;
+    pool_gt(pool_gt&&) = delete;
+    pool_gt(pool_gt const&) = delete;
+    pool_gt& operator=(pool_gt const&) = delete;
 
     pool_gt& operator=(pool_gt&& other) noexcept {
         std::swap(capacity_, other.capacity_);
@@ -118,13 +142,17 @@ template <typename element_at> struct pool_gt {
         elements_ = (element_at*)mem;
         free_offsets_ = (std::size_t*)(elements_ + n);
         free_count_ = capacity_ = n;
+        std::uninitialized_default_construct(elements_, elements_ + capacity_);
         std::iota(free_offsets_, free_offsets_ + n, 0ul);
         return true;
     }
 
-    ~pool_gt() noexcept { std::free(elements_); }
-    element_at& operator[](std::size_t i) noexcept { return elements_[i]; }
-    element_at* alloc() noexcept { return !free_count_ ? nullptr : elements_ + free_offsets_[free_count_--]; }
+    ~pool_gt() noexcept {
+        if constexpr (!std::is_trivially_destructible<element_at>())
+            std::destroy_n(elements_, capacity_);
+        std::free(elements_);
+    }
+    element_at* alloc() noexcept { return free_count_ ? elements_ + free_offsets_[--free_count_] : nullptr; }
     void release(element_at* released) noexcept { free_offsets_[free_count_++] = released - elements_; }
 };
 
