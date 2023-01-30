32a161bc411a5405a972c0518d84122160edd38c
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-01-30 18:23:36 +0400

    Fix: Measuring request latency

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -2,86 +2,102 @@ import socket
 import errno
 import time
 from multiprocessing import Process, Value
+from dataclasses import dataclass
+from inspect import cleandoc as I
 
 from tqdm import tqdm
 
 
-def _do_benchmark(callable, count_cycles: int, debug: bool = False):
-    transmits = 0
-    failures = 0
+@dataclass
+class Stats:
+    transmits_success: int = 0
+    transmits_failed: int = 0
+    requests: int = 0
+    latency_secs: float = 0
+    total_secs: float = 0
+
+    def __repr__(self) -> str:
+        failures_p = self.transmits_failed * 100.0 / \
+            (self.transmits_success + self.transmits_failed)
+        bandwidth = self.requests / self.total_secs
+        result = f'''
+        - Took {self.total_secs:.1f} seconds
+        - Performed {self.transmits_success:,} transmits_success
+        - Recorded {failures_p:.3%} transmits_failed
+        - Mean latency is {self.latency_secs:.1f} microseconds
+        - Mean speed is {bandwidth:.1f} requests/s
+        '''
+        return I(result)
+
+
+def benchmark(callable, count_cycles: int, debug: bool = False) -> Stats:
+    stats = Stats()
+    tasks_range = range(count_cycles)
     if debug:
-        for _ in tqdm(range(count_cycles)):
-            callable()
-            transmits += 1
-    else:
-        for _ in range(count_cycles):
-            try:
-                callable()
-                transmits += 1
-            except:
-                failures += 1
-    return (transmits, failures)
+        tasks_range = tqdm(tasks_range)
+
+    for _ in range(count_cycles):
+        t1 = time.monotonic_ns()
+        successes = callable()
+        stats.requests += successes
+        stats.transmits_success += successes != 0
+        stats.transmits_failed += successes == 0
+        t2 = time.monotonic_ns()
+        stats.total_secs += (t2 - t1) / 1e9
+
+    stats.latency_secs = stats.total_secs / stats.transmits_success
+    return stats
 
 
-def _finalize_benchmark(transmits, failures, duration, threads=1):
-    failures_p = failures * 100.0 / (transmits + failures)
-    latency = ((duration * 1_000_000 / (transmits / threads))
-               if transmits else float('inf'))
-    speed = transmits / duration
-
-    print(f'- Took {duration:.1f} seconds')
-    print(f'- Performed {transmits:,} transmissions')
-    print(f'- Recorded {failures_p:.3%} failures')
-    print(f'- Mean latency is {latency:.1f} microseconds')
-    print(f'- Mean speed is {speed:.1f} requests/s')
-
-
-def benchmark_request_single(callable, count_cycles: int = 100_000, debug: bool = False):
-    start_time = time.time()
-    transmits, failures = _do_benchmark(callable, count_cycles, debug)
-    duration = time.time() - start_time
-    _finalize_benchmark(transmits, failures, duration)
-
-
-def benchmark_request(callable, process_cnt: int = 1, count_cycles: int = 100_000, debug: bool = False):
-
-    if process_cnt == 1:
-        return benchmark_request_single(callable=callable, count_cycles=count_cycles, debug=debug)
-
-    transmits = Value('i', 0)
-    failures = Value('i', 0)
-
-    def run():
-        t, f = _do_benchmark(callable, count_cycles//process_cnt, debug)
-        transmits.value += t
-        failures.value += f
-
-    procs = []
-    for _ in range(process_cnt):
-        procs.append(Process(target=run))
-
-    start_time = time.time()
-    [x.start() for x in procs]
-    [x.join() for x in procs]
-    duration = time.time() - start_time
-
-    _finalize_benchmark(transmits.value, failures.value, duration, process_cnt)
-
-
-async def benchmark_request_async(callable, *args, count_cycles: int = 10_000):
-    failures = 0
-    transmits = 0
+async def benchmark_async(callable, *args, count_cycles: int = 10_000):
+    return
+    transmits_failed = 0
+    transmits_success = 0
 
     start_time = time.time()
     for _ in range(count_cycles):
         try:
             await callable(*args)
-            transmits += 1
+            transmits_success += 1
         except Exception as e:
-            failures += 1
+            transmits_failed += 1
     duration = time.time() - start_time
+    _finalize_benchmark(transmits_success, transmits_failed, duration)
 
-    _finalize_benchmark(transmits, failures, duration)
+
+def benchmark_parallel(callable, process_count: int = 1, count_cycles: int = 100_000, debug: bool = False):
+
+    if process_count == 1:
+        return benchmark(callable=callable, count_cycles=count_cycles, debug=debug)
+
+    transmits_success = Value('i', 0)
+    transmits_failed = Value('i', 0)
+    requests = Value('i', 0)
+    total_secs = Value('f', 0)
+    mean_latency_secs = Value('f', 0)
+
+    def run():
+        stats = benchmark(callable, count_cycles, debug)
+        transmits_success.value += stats.transmits_success
+        transmits_failed.value += stats.transmits_failed
+        requests.value += stats.requests
+        total_secs.value += stats.total_secs
+        mean_latency_secs.value += stats.latency_secs
+
+    procs = []
+    for _ in range(process_count):
+        procs.append(Process(target=run))
+
+    [x.start() for x in procs]
+    [x.join() for x in procs]
+
+    return Stats(
+        transmits_success=transmits_success.value,
+        transmits_failed=transmits_failed.value,
+        requests=requests.value,
+        latency_secs=mean_latency_secs.value / process_count,
+        total_secs=total_secs.value,
+    )
 
 
 def socket_is_closed(sock: socket.socket) -> bool:
--- benchmark/fast_api_client.py
+++ benchmark/fast_api_client.py
@@ -6,7 +6,7 @@ import random
 import requests
 from websocket import create_connection
 
-from benchmark import benchmark_request
+import benchmark
 
 
 def request_sum():
@@ -15,6 +15,7 @@ def request_sum():
     result = requests.get(f'http://127.0.0.1:8000/sum?a={a}&b={b}').text
     c = int(result)
     assert a + b == c, 'Wrong sum'
+    return 1
 
 
 def request_sum_ws_reusing(socket):
@@ -24,24 +25,30 @@ def request_sum_ws_reusing(socket):
     result = socket.recv()
     c = struct.unpack('<I', result)[0]
     assert a + b == c, 'Wrong sum'
+    return 1
+
 
 def make_socket():
     return create_connection('ws://127.0.0.1:8000/sum-ws')
+
 
 def request_sum_ws():
     socket = make_socket()
     request_sum_ws_reusing(socket)
     socket.close()
 
+
 if __name__ == '__main__':
 
-    print('Will benchmark classic REST API')
-    benchmark_request(request_sum)
+    for p in [1, 4]:
+        print('Will benchmark classic REST API')
+        benchmark.benchmark_parallel(request_sum, process_count=p)
 
-    print('Will benchmark WebSockets')
-    benchmark_request(request_sum_ws)
+        print('Will benchmark WebSockets')
+        benchmark.benchmark_parallel(request_sum_ws, process_count=p)
 
-    print('Will benchmark reused WebSockets')
-    socket = make_socket()
-    benchmark_request(lambda: request_sum_ws_reusing(socket))
-    socket.close()
+        print('Will benchmark reused WebSockets')
+        socket = make_socket()
+        benchmark.benchmark_parallel(
+            lambda: request_sum_ws_reusing(socket), process_count=p)
+        socket.close()
