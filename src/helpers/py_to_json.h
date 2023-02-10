54bb147f1a91365b7cf9a1f9cc8d2128385dd392
Ishkhan Nazaryan <105867377+ishkhan42@users.noreply.github.com>
2023-02-10 08:42:26 +0000

    Fix: Get utf8 size in bytes, not code points.

--- src/helpers/py_to_json.h
+++ src/helpers/py_to_json.h
@@ -147,7 +147,9 @@ static int calculate_size_as_str(PyObject* obj) {
     else if (PyBytes_Check(obj))
         return snprintf(NULL, 0, "\"%s\"", PyBytes_AsString(obj));
     else if (PyUnicode_Check(obj)) {
-        return PyUnicode_GET_LENGTH(obj) + 2;
+        Py_ssize_t byte_size = 0;
+        PyUnicode_AsUTF8AndSize(obj, byte_size);
+        return byte_size + 2;
     } else if (PySequence_Check(obj)) {
         size_t size = 2;
         if (PySequence_Length(obj)) {
