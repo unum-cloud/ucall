aed0707a5605f2f2064a73d2231d9c1d613a92d8
Ishkhan Nazaryan <105867377+ishkhan42@users.noreply.github.com>
2023-02-23 06:45:05 +0000

    Fix: Return proper error

--- examples/sum/ujrpc_server.cpp
+++ examples/sum/ujrpc_server.cpp
@@ -17,7 +17,7 @@ static void sum(ujrpc_call_t call, ujrpc_data_t) {
     bool got_a = ujrpc_param_named_i64(call, "a", 0, &a);
     bool got_b = ujrpc_param_named_i64(call, "b", 0, &b);
     if (!got_a || !got_b)
-        return ujrpc_call_reply_error(call, 1, "Missing integer argument a and/or b", 0);
+        return ujrpc_call_reply_error_invalid_params(call);
 
     std::to_chars_result print = std::to_chars(&c_str[0], &c_str[0] + sizeof(c_str), a + b, 10);
     ujrpc_call_reply_content(call, &c_str[0], print.ptr - &c_str[0]);
