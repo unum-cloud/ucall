5cdcb2145256cf321ee3008b9a54bde676d9fa17
Ishkhan Nazaryan <105867377+ishkhan42@users.noreply.github.com>
2023-08-09 10:11:18 +0000

    Docs: Document `drop_embedded_n`

--- src/headers/containers.hpp
+++ src/headers/containers.hpp
@@ -233,6 +233,9 @@ class exchange_pipes_t {
     char* next_input_address() noexcept { return input_.embedded; }
     std::size_t next_input_length() const noexcept { return ram_page_size_k; }
 
+    /**
+     * @brief Discards the first 'cnt' elements from the embedded buffer.
+     */
     void drop_embedded_n(size_t cnt) noexcept {
         if (cnt > input_.embedded_used)
             return release_inputs();
