5e9ab808661bd2a85a6996f4789ad66ec0a0237b
Ishkhan Nazaryan <105867377+ishkhan42@users.noreply.github.com>
2023-09-12 11:30:25 +0000

    Build: General Fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import shutil
 from os.path import dirname
 import multiprocessing
 import subprocess
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
 __version__ = open("VERSION", "r").read().strip()
@@ -27,7 +27,8 @@ class CMakeExtension(Extension):
 class CMakeBuild(build_ext):
     def build_extension(self, ext):
         self.parallel = multiprocessing.cpu_count() // 2
-        extension_dir = os.path.abspath(dirname(self.get_ext_fullpath("ucall")))
+        extension_dir = os.path.abspath(
+            dirname(self.get_ext_fullpath(ext.name)))
 
         # required for auto-detection & inclusion of auxiliary 'native' libs
         if not extension_dir.endswith(os.path.sep):
@@ -42,13 +43,15 @@ class CMakeBuild(build_ext):
         # Adding CMake arguments set as environment variable
         # (needed e.g. to build for ARM OSx on conda-forge)
         if "CMAKE_ARGS" in os.environ:
-            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
+            cmake_args += [
+                item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
 
         if sys.platform.startswith("darwin"):
             # Cross-compile support for macOS - respect ARCHFLAGS if set
             archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
             if archs:
-                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
+                cmake_args += [
+                    "-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
 
         # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
         # across all generators.
@@ -68,24 +71,27 @@ class CMakeBuild(build_ext):
             ["cmake", "--build", ".", "--target", build_name] + build_args
         )
 
-        # Add these lines to copy the .so file to the expected directory
-        if sys.platform.startswith("darwin"):
-            suffix = "darwin.so"
-        elif sys.platform.startswith("linux"):
-            suffix = "linux-gnu.so"
-        else:
-            raise RuntimeError(f"Unsupported platform: {sys.platform}")
+        # # Add these lines to copy the .so file to the expected directory
+        # if sys.platform.startswith("darwin"):
+        #     suffix = "darwin.so"
+        # elif sys.platform.startswith("linux"):
+        #     suffix = "linux-gnu.so"
+        # else:
+        #     raise RuntimeError(f"Unsupported platform: {sys.platform}")
 
-        backend = ext.name.split(".")[-1]
-        submodules_folder = os.path.join(extension_dir, "ucall")
-        if not os.path.exists(submodules_folder):
-            os.mkdir(submodules_folder)
-        expected_output = os.path.join(
-            submodules_folder,
-            f"{backend}.cpython-{sys.version_info.major}{sys.version_info.minor}-{suffix}",
-        )
-        actual_output = os.path.join(extension_dir, build_name + ".so")
-        shutil.copyfile(actual_output, expected_output)
+        # backend = ext.name.split(".")[-1]
+        # submodules_folder = os.path.join(extension_dir, ext.name)
+        # if not os.path.exists(submodules_folder):
+        #     os.mkdir(submodules_folder)
+        # expected_output = os.path.join(
+        #     submodules_folder,
+        #     f"{backend}.cpython-{sys.version_info.major}{sys.version_info.minor}-{suffix}",
+        # )
+        # actual_output = os.path.join(extension_dir, build_name + ".so")
+        # shutil.copyfile(actual_output, expected_output)
+
+    def run(self):
+        build_ext.run(self)
 
 
 ext_modules = [CMakeExtension("ucall.posix")]
@@ -127,8 +133,8 @@ setup(
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: System :: Networking",
     ],
-    packages=find_packages(where="./python"),  # Your Python packages are under ./python
-    package_dir={"": "./python"},  # Tell setuptools that packages are under ./python
+    packages=["ucall"],
+    package_dir={"": "python"},
     ext_modules=ext_modules,
     cmdclass={"build_ext": CMakeBuild},
     entry_points={"console_scripts": ["ucall=ucall.cli:cli"]},
